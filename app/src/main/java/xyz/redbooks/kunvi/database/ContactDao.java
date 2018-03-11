65f5247391b7a2c53cfe602dd115b00eb6d70b17
h4rdw1r3 <vinaugust6@gmail.com>
2018-03-11 19:55:38 +0530

    Added Data Access Object for room persitence library entity Contact

---
+++ app/src/main/java/xyz/redbooks/kunvi/database/ContactDao.java
@@ -0,0 +1,30 @@
+package xyz.redbooks.kunvi.database;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+import xyz.redbooks.kunvi.Contact;
+
+@Dao
+public interface ContactDao {
+
+    @Query("SELECT name FROM Contact")
+    List<Contact> getAllContactsName();
+
+    @Query("SELECT mobileNumber FROM contact")
+    List<Contact> getAllContactsNumber();
+
+    @Query("SELECT * FROM Contact WHERE id = :id")
+    Contact getContactById(int id);
+
+    @Insert
+    void insertContact(Contact contact);
+
+    @Delete
+    void Delete(Contact contact);
+
+}
