8c770380be3541a55fc50d8b450909be1b8b5165
h4rdw1r3 <vinaugust6@gmail.com>
2018-03-11 20:07:21 +0530

    Created app database

---
+++ app/src/main/java/xyz/redbooks/kunvi/database/AppDatabase.java
@@ -0,0 +1,26 @@
+package xyz.redbooks.kunvi.database;
+
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+import xyz.redbooks.kunvi.Contact;
+
+@Database(entities = {Contact.class},version = 1, exportSchema = false)
+public abstract class AppDatabase extends RoomDatabase {
+
+    private static AppDatabase INSTANCE;
+    public abstract ContactDao contactDao();
+
+    public static AppDatabase getAppDatabase(Context context) {
+        if(INSTANCE == null) {
+            INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                    AppDatabase.class, "appDB").allowMainThreadQueries().build();
+        }
+
+        return INSTANCE;
+    }
+
+}
