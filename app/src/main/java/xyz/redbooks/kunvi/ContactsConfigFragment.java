430e2274b6fbee0a33b9b04617269505af0809d1
h4rdw1r3 <vinaugust6@gmail.com>
2018-03-14 22:18:02 +0530

    Added contacts setting fragment(flexible activity)

---
+++ app/src/main/java/xyz/redbooks/kunvi/ContactsConfigFragment.java
@@ -0,0 +1,29 @@
+package xyz.redbooks.kunvi;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ContactsConfigFragment extends Fragment {
+
+
+    public ContactsConfigFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_contacts_config, container, false);
+
+
+        return view ;
+    }
+
+}
