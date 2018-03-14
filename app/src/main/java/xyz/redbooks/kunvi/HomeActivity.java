3ac026296a5f394194295057aa5dbc1000eba63b
h4rdw1r3 <vinaugust6@gmail.com>
2018-03-14 17:54:13 +0530

    Added Navigation view

--- app/src/main/java/xyz/redbooks/kunvi/HomeActivity.java
+++ app/src/main/java/xyz/redbooks/kunvi/HomeActivity.java
@@ -1,12 +1,22 @@
 package xyz.redbooks.kunvi;
 
 import android.hardware.SensorManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import com.squareup.seismic.ShakeDetector;
@@ -18,17 +28,102 @@ import java.util.Calendar;
 public class HomeActivity extends AppCompatActivity {
 
     FragmentManager fm;
+    private ActionBarDrawerToggle drawerToggle;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        setUpToolBar();
+
+        drawerLayout = findViewById(R.id.drawer);
+
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                //Checking if the item is in checked state or not, if not make it in checked state
+                if(item.isChecked())
+                    item.setChecked(false);
+                else
+                    item.setChecked(true);
+
+                //Closing drawer on item click
+                drawerLayout.closeDrawers();
+
+                //Check to see which item was being clicked and perform appropriate action
+                switch (item.getItemId()){
+
+                    case R.id.home:
+                        Snackbar.make(findViewById(R.id.drawer), "Message from Home", Snackbar.LENGTH_SHORT).show();
+                        return true;
+
+                    case R.id.trusted_contact_menu:
+                        return true;
+
+                    case R.id.about:
+                        return true;
+
+                    default:
+                        return true;
+                }
+            }
+        });
+
+        drawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open, R.string.drawer_close){
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Code here will be triggered once the drawer closes as we don't want anything to happen so we leave this blank
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Code here will be triggered once the drawer open as we don't want anything to happen so we leave this blank
+
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
+        // add the tip of the day fragment
         fm = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fm.beginTransaction();
 
         Fragment fragment = new TipOfTheDayFragment();
-        fragmentTransaction.add(R.id.fragment_container, fragment).commit();
+        fragmentTransaction.replace(R.id.fragment_container, fragment).commit();
 
     }
+
+    private void setUpToolBar() {
+        toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (isNavDrawerOpen()) {
+            closeNavDrawer();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    protected boolean isNavDrawerOpen() {
+        return drawerLayout != null && drawerLayout.isDrawerOpen(GravityCompat.START);
+    }
+
+    protected void closeNavDrawer() {
+        if (drawerLayout != null) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+
 }
