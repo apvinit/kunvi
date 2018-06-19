d04e564b1d48be4c14b7efcea982dd7cf856182f
h4rdw1r3 <vinaugust6@gmail.com>
2018-06-19 12:28:54 +0530

    Refactored

--- README.md
+++ README.md
@@ -3,8 +3,7 @@ An app for alerting trusted contacts with location coordinates and google maps l
 Kun Vi in means __With You__ in English. It is aimed to be used by women.
 
 ## Features
-* Safety Tip of the day.
 * Alerting trusted contacts with location coordinates.
 * Works in background.
 * 4 times power button triggers alert action
-* Starts a fake incoming call
+* Sends call to the first contact int the list
---
+++ app/src/main/java/xyz/redbooks/kunvi/core/BootReceiver.java
@@ -0,0 +1,16 @@
+package xyz.redbooks.kunvi.core;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if(intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+            Intent service = new Intent(context, MyService.class);
+            context.startService(service);
+        }
+    }
+}
